f49235c9f1ce3476045a12091275ec05ca6437bf
dennysferrer <dennys.ferrer@gmail.com>
2025-03-30 20:45:06 -0500

    Aprendimos a trabajar con los eventos click en los botones de los formularios

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragoballPageComponent } from './pages/dragonball/dragoball-page/dragoball-page.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path: 'hero',
         component: HeroPageComponent
+    },
+    {
+        path: 'dragonball',
+        component: DragoballPageComponent
     },
     {
         path: '**',
